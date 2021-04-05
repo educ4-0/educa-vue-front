2b985dd3edfe1020a5622720ca75f3705cb3f848
Iuri Jordan <jordaniiuri@outlook.com>
2021-04-05 14:39:29 -0300

    Edição na tela Profile e adição do letreiro EDUCA

--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -21,53 +21,51 @@
         <div class="card text-center">
           <div class="card-body">
             <img
-              src="../../assets/images/users/avatar-1.jpg"
+              src="../../assets/images/users/avatar-7.jpg"
               class="rounded-circle avatar-xl img-thumbnail"
               alt="profile-image"
             />
 
-            <h4 class="mt-3 mb-0">Nik G. Patel</h4>
-            <p class="text-muted">@webdesigner</p>
+            <h4 class="mt-3 mb-0">Ricardo S. Nogueira</h4>
+            <p class="text-muted">@ricnogueira</p>
 
             <button
               type="button"
               class="btn btn-success btn-xs waves-effect mb-2 waves-light"
             >
-              Follow
+              Seguir
             </button>
             <button
               type="button"
               class="btn btn-danger btn-xs waves-effect mb-2 waves-light"
             >
-              Message
+              Mensagem
             </button>
 
             <div class="text-left mt-3">
-              <h4 class="font-13 text-uppercase">About Me :</h4>
+              <h4 class="font-13 text-uppercase">Sobre mim:</h4>
               <p class="text-muted font-13 mb-3">
-                Hi I'm Johnathn Deo,has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type.
+                Olá sou o Ricardo Nogueira...
               </p>
 
               <div class="table-responsive">
                 <table class="table table-borderless table-sm">
                   <tbody>
                     <tr>
-                      <th scope="row">Full Name :</th>
-                      <td class="text-muted">Nik G. Patel</td>
+                      <th scope="row">Nome completo:</th>
+                      <td class="text-muted">Ricardo Nogueira</td>
                     </tr>
                     <tr>
-                      <th scope="row">Mobile :</th>
-                      <td class="text-muted">(123) 123 1234</td>
+                      <th scope="row">Telefone :</th>
+                      <td class="text-muted">(88) 9 9999-9999</td>
                     </tr>
                     <tr>
                       <th scope="row">Email :</th>
-                      <td class="text-muted">user@email.domain</td>
+                      <td class="text-muted">ricardo.nogueira@gmail.com</td>
                     </tr>
                     <tr>
-                      <th scope="row">Location :</th>
-                      <td class="text-muted">USA</td>
+                      <th scope="row">Endereço :</th>
+                      <td class="text-muted">Rua Santos Dumont</td>
                     </tr>
                   </tbody>
                 </table>
@@ -109,14 +107,14 @@
 
         <div class="card">
           <div class="card-body">
-            <h4 class="header-title">Skills</h4>
+            <h4 class="header-title">Avaliações</h4>
             <p class="sub-header mb-3">
-              Everyone realizes why a new common language would be desirable
+              Progresso das avaliações feita pelos alunos
             </p>
 
             <div class="pt-1">
               <h6 class="text-uppercase mt-0">
-                HTML5 <span class="float-right">90%</span>
+                Interação com a turma <span class="float-right">90%</span>
               </h6>
               <div class="progress progress-sm m-0">
                 <div
@@ -134,7 +132,7 @@
 
             <div class="mt-2 pt-1">
               <h6 class="text-uppercase">
-                PHP <span class="float-right">67%</span>
+                Contéudos atualizados <span class="float-right">67%</span>
               </h6>
               <div class="progress progress-sm m-0">
                 <div
@@ -152,7 +150,7 @@
 
             <div class="mt-2 pt-1">
               <h6 class="text-uppercase">
-                WordPress <span class="float-right">48%</span>
+                Aulas dinâmicas <span class="float-right">48%</span>
               </h6>
               <div class="progress progress-sm m-0">
                 <div
@@ -170,7 +168,7 @@
 
             <div class="mt-2 pt-1">
               <h6 class="text-uppercase">
-                Laravel <span class="float-right">95%</span>
+                Outros <span class="float-right">95%</span>
               </h6>
               <div class="progress progress-sm m-0">
                 <div
@@ -186,9 +184,9 @@
               </div>
             </div>
 
-            <div class="mt-2 pt-1">
+            <!-- <div class="mt-2 pt-1">
               <h6 class="text-uppercase">
-                ReactJs <span class="float-right">72%</span>
+                Outros <span class="float-right">72%</span>
               </h6>
               <div class="progress progress-sm m-0">
                 <div
@@ -202,7 +200,7 @@
                   <span class="sr-only">72% Complete</span>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
@@ -218,7 +216,7 @@
                   aria-expanded="true"
                   class="nav-link active ml-0"
                 >
-                  <i class="mdi mdi-face-profile mr-1"></i>About Me
+                  <i class="mdi mdi-face-profile mr-1"></i>Sobre mim
                 </a>
               </li>
               <li class="nav-item">
@@ -228,14 +226,14 @@
                   aria-expanded="false"
                   class="nav-link"
                 >
-                  <i class="mdi mdi-cog mr-1"></i>Settings
+                  <i class="mdi mdi-cog mr-1"></i>Configurações
                 </a>
               </li>
             </ul>
 
             <div class="tab-content">
               <div class="tab-pane show active" id="about-me">
-                <h5 class="mb-4 text-uppercase">Experience</h5>
+                <h5 class="mb-4 text-uppercase">Experiência</h5>
 
                 <ul class="list-unstyled timeline-sm">
                   <li class="timeline-sm-item">
@@ -274,17 +272,17 @@
                   </li>
                 </ul>
 
-                <h5 class="mb-3 mt-5 text-uppercase">Projects</h5>
+                <h5 class="mb-3 mt-5 text-uppercase">Projetos</h5>
                 <div class="table-responsive">
                   <table class="table table-borderless table-nowrap mb-0">
                     <thead class="thead-light">
                       <tr>
                         <th>#</th>
-                        <th>Project Name</th>
-                        <th>Start Date</th>
-                        <th>Due Date</th>
+                        <th>Nome do projeto</th>
+                        <th>Data de início</th>
+                        <th>Data de vencimento</th>
                         <th>Status</th>
-                        <th>Clients</th>
+                        <th>Clientes</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -294,7 +292,7 @@
                         <td>01/01/2015</td>
                         <td>10/15/2018</td>
                         <td>
-                          <span class="badge badge-info">Work in Progress</span>
+                          <span class="badge badge-info">Trabalho em progresso</span>
                         </td>
                         <td>Halette Boivin</td>
                       </tr>
@@ -342,29 +340,29 @@
               <div class="tab-pane" id="settings">
                 <form>
                   <h5 class="mb-3 text-uppercase bg-light p-2">
-                    <i class="mdi mdi-account-circle mr-1"></i> Personal Info
+                    <i class="mdi mdi-account-circle mr-1"></i> Informação pessoal
                   </h5>
 
                   <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
-                        <label for="firstname">First Name</label>
+                        <label for="firstname">Primeiro nome</label>
                         <input
                           type="text"
                           class="form-control"
                           id="firstname"
-                          placeholder="Enter first name"
+                          placeholder="Insira seu primeiro nome"
                         />
                       </div>
                     </div>
                     <div class="col-md-6">
                       <div class="form-group">
-                        <label for="lastname">Last Name</label>
+                        <label for="lastname">Último nome</label>
                         <input
                           type="text"
                           class="form-control"
                           id="lastname"
-                          placeholder="Enter last name"
+                          placeholder="Insira seu último nome"
                         />
                       </div>
                     </div>
@@ -378,7 +376,7 @@
                           class="form-control"
                           id="userbio"
                           rows="4"
-                          placeholder="Write something..."
+                          placeholder="Escreva algo..."
                         ></textarea>
                       </div>
                     </div>
@@ -387,36 +385,36 @@
                   <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
-                        <label for="useremail">Email Address</label>
+                        <label for="useremail">Email</label>
                         <input
                           type="email"
                           class="form-control"
                           id="useremail"
-                          placeholder="Enter email"
+                          placeholder="Insira o email"
                         />
                         <span class="form-text text-muted"
                           ><small
-                            >If you want to change email please
+                            >Se você deseja alterar o email
                             <a href="javascript: void(0);">click</a>
-                            here.</small
+                            aqui.</small
                           ></span
                         >
                       </div>
                     </div>
                     <div class="col-md-6">
                       <div class="form-group">
-                        <label for="userpassword">Password</label>
+                        <label for="userpassword">Senha</label>
                         <input
                           type="password"
                           class="form-control"
                           id="userpassword"
-                          placeholder="Enter password"
+                          placeholder="Insira a senha"
                         />
                         <span class="form-text text-muted"
                           ><small
-                            >If you want to change password please
+                            >Se você deseja alterar a senha
                             <a href="javascript: void(0);">click</a>
-                            here.</small
+                            aqui.</small
                           ></span
                         >
                       </div>
@@ -424,17 +422,17 @@
                   </div>
 
                   <h5 class="mb-3 text-uppercase bg-light p-2">
-                    <i class="mdi mdi-office-building mr-1"></i> Company Info
+                    <i class="mdi mdi-office-building mr-1"></i> Informação da empresa
                   </h5>
                   <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
-                        <label for="companyname">Company Name</label>
+                        <label for="companyname">Nome da empresa</label>
                         <input
                           type="text"
                           class="form-control"
                           id="companyname"
-                          placeholder="Enter company name"
+                          placeholder="Insira o nome da empresa"
                         />
                       </div>
                     </div>
@@ -445,7 +443,7 @@
                           type="text"
                           class="form-control"
                           id="cwebsite"
-                          placeholder="Enter website url"
+                          placeholder="Insira o link do seu site"
                         />
                       </div>
                     </div>
@@ -576,7 +574,7 @@
                       type="submit"
                       class="btn btn-success waves-effect waves-light mt-2"
                     >
-                      <i class="mdi mdi-content-save"></i> Save
+                      <i class="mdi mdi-content-save"></i> Salvar
                     </button>
                   </div>
                 </form>
